ab57d7722cc00943a7e7590aed9dde2e400682aa
ZavhorodniiIllia <ogorod-illya@mail.ru>
2017-03-24 11:06:23 +0200

    lab1

---
+++ menu.py
@@ -0,0 +1,20 @@
+class Menu:
+    @staticmethod
+    def base_menu():
+        print ('Program accounting profits and expenses\n--------------')
+        print ('Choose your path:')
+        print ('1)Open existing wallet')
+        print ('2)Create new wallet')
+        print ('3)Exit')
+
+    @staticmethod
+    def open_menu():
+        print ('Choose the wallet:')
+
+    @staticmethod
+    def create_menu():
+        print ('Enter wallet`s name:')
+
+    @staticmethod
+    def wallet_menu():
+
